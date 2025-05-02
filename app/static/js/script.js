94c47c1963a41bfabf4df032637e82e3685faad1
Mangomigu <suzhenyi1994@gmail.com>
2025-05-02 16:55:26 +0800

    Add home.html template with diary grid and modal upload

---
+++ app/static/js/script.js
@@ -0,0 +1,10 @@
+// --- Upload Modal 控制 ---
+document.querySelector('.new-diary-btn').addEventListener('click', () => {
+    document.getElementById('uploadModal').style.display = 'block';
+});
+document.getElementById('closeModal').addEventListener('click', () => {
+    document.getElementById('uploadModal').style.display = 'none';
+});
+
+
+
